f6ed242c8c16dc2bb4502c4145871eb2a00d1ba3
chomskim <csknova@gmail.com>
2021-03-11 10:30:26 +0900

    add uml-diagram

--- HeadFirstDesignPattern/src/headfirst/designpatterns/strategy/MiniDuckSimulator.java
+++ HeadFirstDesignPattern/src/headfirst/designpatterns/strategy/MiniDuckSimulator.java
@@ -1,19 +1,19 @@
 package headfirst.designpatterns.strategy;
 
 public class MiniDuckSimulator {
- 
+
 	public static void main(String[] args) {
- 
-		MallardDuck	mallard = new MallardDuck();
-		RubberDuck	rubberDuckie = new RubberDuck();
-		DecoyDuck	decoy = new DecoyDuck();
- 
-		Duck	 model = new ModelDuck();
+
+		Duck	mallard = new MallardDuck();
+		Duck	rubberDuckie = new RubberDuck();
+		Duck	decoy = new DecoyDuck();
+
+		Duck	model = new ModelDuck();
 
 		mallard.performQuack();
 		rubberDuckie.performQuack();
 		decoy.performQuack();
-   
+
 		model.performFly();	
 		model.setFlyBehavior(new FlyRocketPowered());
 		model.performFly();
