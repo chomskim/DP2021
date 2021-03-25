57d648aad5fec66f1eb9b2a0a83904fb7fcf34a6
chomskim <csknova@gmail.com>
2021-03-25 11:20:31 +0900

    commit digitalclock

---
+++ DigitalClockFXML/src/hufs/ces/digitalclock/DigitalClockController.java
@@ -0,0 +1,45 @@
+package hufs.ces.digitalclock;
+
+import java.util.Calendar;
+import java.util.Formatter;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
+
+public class DigitalClockController {
+	
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    private Label lblTime;
+
+    @FXML
+    void startAction(ActionEvent event) {
+		setTimeDisplay();
+	    // Create an animation for a running clock
+	    Timeline animation = new Timeline(new KeyFrame(Duration.millis(1000), e->setTimeDisplay()));
+	    animation.setCycleCount(Timeline.INDEFINITE);
+	    animation.play(); // Start animation
+    }
+    
+	void setTimeDisplay(){
+		
+		Calendar calendar = Calendar.getInstance();
+
+		StringBuilder timeText = new StringBuilder();
+		Formatter buildTime = new Formatter(timeText);
+		buildTime.format("%tT", calendar);
+		lblTime.setText(timeText.toString());
+
+		StringBuilder dateText = new StringBuilder();
+		Formatter buildDate = new Formatter(dateText);
+		buildDate.format("%1$tY.%1$tm.%1$td", calendar);
+		lblDate.setText(dateText.toString());
+	}
+
+}
